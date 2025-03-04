b9856e7eddeb6a03f69a756a8f623497462f6b78
DULCE_MARIA_TOLENTINO_DE_ANDA <a.22308051281367@cbtis128.edu.mx>
2025-03-04 16:44:19 +0000

    imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ejemplo imagen circular"),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+          centerTitle: true,
+          backgroundColor: Color(0xff720ad4), // Color morado
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(height: 50), // Espacio para la imagen
+            Center(
+              // Centrar la imagen
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://raw.githubusercontent.com/Dulce-Maria-Tolentino-De-Anda/p10-5autonma1367/refs/heads/main/se%C3%B1orbarbon.jpg'), // Reemplaza con la URL de tu imagen
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 60), // Espacio entre la imagen y el texto
+            Text(
+              "Dulce Tolentino 1367",
+              style: TextStyle(fontSize: 30, color: Color(0xff5c62ba)),
+            ),
+          ],
         ),
       ),
     );
